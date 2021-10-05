91f89994964a9ed9a3afc0c994675fb8ca4d1322
JeonMinJae <ssi02014@naver.com>
2021-10-05 23:19:10 +0900

    Add(nav): 폴더 구조 변화 #10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
 import React, { useEffect } from 'react';
 import type { NextPage } from 'next'
 import { Container } from 'styles/_common';
-import Example from 'components/Example';
-import ProjectBoard from 'containers/ProjectBoard';
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    if (window.innerWidth < 1024) {
-      console.log(window.innerWidth);
-      alert("해당 웹은 브라우저 너비가 1024px 이상에서 정상 작동합니다. 브라우저 너비를 키워주시기 바랍니다.");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.innerWidth < 1024) {
+  //     console.log(window.innerWidth);
+  //     alert("해당 웹은 브라우저 너비가 1024px 이상에서 정상 작동합니다. 브라우저 너비를 키워주시기 바랍니다.");
+  //   }
+  // }, []);
   
   return (
     <Container>
-      <ProjectBoard />
-      {/* <Example /> */}
     </Container>
   )
 }
---
+++ pages/projectBoard/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectBoard from 'components/ProjectBoard';
+import { Container } from 'styles/_common';
+
+const projectBoard = () => {
+  return (
+    <Container>
+      <ProjectBoard />
+    </Container>
+  );
+};
+
+export default projectBoard;
