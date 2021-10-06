0f78b598505bdc19c50216af7c9216f4d06577f2
JeonMinJae <ssi02014@naver.com>
2021-10-06 18:07:39 +0900

    Add(nav): Dimmend 추가

--- styles/common.tsx
+++ styles/common.tsx
@@ -20,3 +20,12 @@ export const InnerContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const Dimmend = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
