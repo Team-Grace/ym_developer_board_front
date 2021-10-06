1bc482100a21e97049e65ce03ecd16240c11b84c
JeonMinJae <ssi02014@naver.com>
2021-10-07 02:22:17 +0900

    Add(calendar): Calendar UI

--- components/Calendar/index.tsx
+++ components/Calendar/index.tsx
@@ -1,18 +1,78 @@
-import React from "react";
-import { CalendarContainer} from './style'
+import React, { useMemo, useState } from "react";
+import { 
+  CalendarContainer, 
+  ContentContainer, 
+  DateContainer, 
+  DateItem,
+  DayOfWeekContainer,
+  ControlContainer,
+  ControlButtonContainer,
+} from './style'
+import moment from 'moment';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 const Calendar = () => {
+  const [getMoment, setGetMoment] = useState(moment());
+  const firstWeek = getMoment.clone().startOf('month').week();
+  const lastWeek = getMoment.clone().endOf('month').week() === 1 ? 53 : getMoment.clone().endOf('month').week();
+  const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  const DateItems = useMemo(() => {
+    let result:React.ReactNode[] = [];
+    
+    for (let i = firstWeek; i <= lastWeek; i++) {
+      const element = (
+        <ContentContainer key={i}>
+          {Array(7).fill(0).map((data, index) => {
+            let days = getMoment.clone().startOf('year').week(i).startOf('week').add(index, 'day');
+
+            if (moment().format('YYYYMMDD') === days.format('YYYYMMDD')) {
+              return (
+                <DateItem key={index} className="today" >
+                  {days.format('D')}
+                </DateItem>
+              );
+            } else if (days.format('MM') !== getMoment.format('MM')) {
+              return (
+                <DateItem key={index} className="prev-dates" >
+                  {days.format('D')}
+                </DateItem>
+              );
+            }
+            return(
+              <DateItem key={index} className="current-dates">
+                {days.format('D')}
+              </DateItem>
+            );
+          })}
+        </ContentContainer>
+      );
+      result = [...result, element];
+    }
+    return result;
+  }, [getMoment]);
+
   return (
     <CalendarContainer>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      <h5>zzz</h5>
-      
+      <ControlContainer>
+        <span>{getMoment.format('YYYY년 MM월')}</span>
+        <ControlButtonContainer>
+          <button onClick={()=>{ setGetMoment(getMoment.clone().subtract(1, 'month')) }}>
+            <MdKeyboardArrowUp />
+          </button>
+          <button onClick={()=>{ setGetMoment(getMoment.clone().add(1, 'month')) }}>
+            <MdKeyboardArrowDown />
+          </button>
+        </ControlButtonContainer>
+      </ControlContainer>
+      <DayOfWeekContainer>
+        {dayOfWeek.map((el, idx) => (
+          <p key={idx}>{el}</p>
+        ))}
+      </DayOfWeekContainer>
+      <DateContainer>
+        {DateItems}
+      </DateContainer>
     </CalendarContainer>
   )
 };
--- components/Calendar/style.tsx
+++ components/Calendar/style.tsx
@@ -3,4 +3,98 @@ import { color } from 'config/colorSystem';
 
 export const CalendarContainer = styled.div`
   width: 90%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: 2px 2px 6px #555555;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+export const ControlContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #2a4d85;
+  width: 100%;
+  height: 12%;
+
+  span {
+    margin-left: 40px;
+    color: #fff;
+    font-size: 24px;
+  }
+
+`;
+
+export const ControlButtonContainer = styled.div`
+  margin-right: 20px;
+  display: flex;
+  
+  button {
+    display: flex;
+    background-color: transparent;
+    color: #fff;
+    border: none;
+    outline: none;
+    font-size: 45px;
+    margin: 0 5px;
+    cursor: pointer;
+
+    &:hover {
+      color: #85b4ff;
+    }
+
+    svg {
+      pointer-events: none;
+    }
+  }
+`;
+
+export const DayOfWeekContainer = styled.div`
+  width: 100%;
+  height: 12%;
+  display: flex;
+  justify-content: center;
+  background-color: #718db9;
+
+  p {
+    width: 14.28%;
+    text-align: center;
+    line-height: 80px;
+    color: #fff;
+    font-size: 20px;
+  }
+`;
+
+export const DateContainer = styled.div`
+  width: 100%;
+  height: 550px;
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const DateItem = styled.button`
+  border: none;
+  outline: none;
+  width: 14.3%;
+  color: black;
+  font-size: 18px;
+  background-color: #fff;
+  cursor: pointer;
+
+  &.today {}
+  &.prev-dates {
+    color: #b4b4b4;
+  }
+  &.current-dates:hover {
+    background-color: #d4d4d4;
+  }
 `;
