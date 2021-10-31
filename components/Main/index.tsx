b40aba5bfa1caab00c68632d8303d103aa56e24b
JeonMinJae <ssi02014@naver.com>
2021-10-31 15:19:35 +0900

    Add(main): init #4

---
+++ components/Main/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <h5>hi</h5>
+      <h5>hi</h5>
+      <h5>hi</h5>
+      <h5>hi</h5>
+      <h5>hi</h5>
+    </>
+  )
+};
+
+export default Main;
