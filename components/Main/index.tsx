71a25daddc93c233dd50deda9bb61fe270426b59
JeonMinJae <ssi02014@naver.com>
2021-10-31 15:39:32 +0900

    Add(main): Title, Content #4

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { MainContainer, MainTitle, MainContent } from './style';
+
 
 const Main = () => {
   return (
-    <>
-      <h5>hi</h5>
-      <h5>hi</h5>
-      <h5>hi</h5>
-      <h5>hi</h5>
-      <h5>hi</h5>
-    </>
+    <MainContainer>
+      <MainTitle>YMDB</MainTitle>
+      <MainContent>
+        <span>YMDB</span> 는 개발자들을 위한 웹 개발자 보드입니다. <br />
+        현재 베타 테스트 버전이며, <span>YMDB</span> 는 모바일을 지원하지 않는 웹 전용 서비스입니다.  <br />
+        아직 부족하지만 지속적으로 기능 개선을 하도록 하겠습니다. <br />
+        감사합니다<br />
+      </MainContent>
+    </MainContainer>
   )
 };
 
--- components/Main/style.tsx
+++ components/Main/style.tsx
@@ -1,2 +1,34 @@
 import styled from 'styled-components';
 import { color } from 'config/colorSystem';
+
+export const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${color.main[500]};
+`;
+
+export const MainTitle = styled.h5`
+  margin: 0 0 20px 0;
+  font-family: 'Luckiest Guy', cursive;
+  font-size: 50px;
+  color: #fff;
+`;
+
+export const MainContent = styled.p`
+  padding: 10px;
+  background-color: #fff;
+  box-shadow: 0 0 4px #a5a5a5;
+  border-radius: 6px;
+  width: 450px;
+  font-size: 12px;
+
+  span {
+    font-family: 'Luckiest Guy', cursive;
+    color: ${color.main[500]};
+    font-size: 18px;
+  }
+`;
